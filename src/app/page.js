0fe2d3a3874518ee73c1aef69703e0094f46e3b7
Ezekiel Adeosun <adeosunezekiel005@gmail.com>
2025-10-08 18:18:56 +0100

    Edited commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar"
 import HeroSection from "@/components/HeroSection"
 import ArticleCards from "@/components/ArticleCards"
 
+import "./globals.css"
+
 export default function Home() {
   return (
     <main>
